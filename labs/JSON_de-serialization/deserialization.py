48480febeadfaf2827c67384b56c368727208995
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2022-01-21 23:43:47 +0100

    arrays json serialization

--- labs/JSON_de-serialization/deserialization.py
+++ labs/JSON_de-serialization/deserialization.py
@@ -3,13 +3,14 @@ import json
 from spacex_mission import SpaceXMission
 
 # JSON object
-spacex_mission_json = '{"mission_name": "Crew-3"}'
+spacex_mission_json = '{"mission_name": "Crew-3", "mission_id": "2"}'
 spacex_mission_dict = json.loads(spacex_mission_json)
 spacex_mission = SpaceXMission(**spacex_mission_dict)
 print(spacex_mission.mission_name)
 
 # JSON array
-spacex_missions_json = '[{"mission_name": "Crew-3"}]'
+spacex_missions_json = '[{"mission_name": "Crew-3", "mission_id": "2"}, {"mission_name": "Inspiration-4", "mission_id": "1"}]'
 spacex_missions_array = json.loads(spacex_missions_json)
 spacex_missions = [SpaceXMission(**mission) for mission in spacex_missions_array]
-print(spacex_missions[0].mission_name)
+for mission in spacex_missions:
+    print(mission.mission_name)
--- labs/JSON_de-serialization/spacex_mission.py
+++ labs/JSON_de-serialization/spacex_mission.py
@@ -1,3 +1,4 @@
 class SpaceXMission():
-    def __init__(self, mission_name):
+    def __init__(self, mission_name, mission_id):
         self.mission_name = mission_name
+        self.mission_id = mission_id
